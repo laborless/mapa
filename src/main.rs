e44bcb188bfc3176e0fa0466e6069c330ddf196d
laborless <wyj1023@gmail.com>
2025-04-07 23:22:33 +0200

    added json support

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ use serde::{Deserialize, Serialize};
 // use serde_json::Result;
 
 #[derive(Serialize, Deserialize)]
+struct MemoryConfig {
+    name: String,
+    origin: String,
+    length: String,
+    attribute: String
+}
+
+#[derive(Serialize, Deserialize)]
 struct SubSection {
     name: Vec<String>,
     address: String,
@@ -29,21 +37,22 @@ struct MemoryMap {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Number of times to greet
-    #[arg(short = 'n', long, default_value_t = 1)]
-    count: u8,
+    /// Enable JSON format output
+    #[arg(short = 'j', long, action = clap::ArgAction::SetTrue)]
+    json: bool,
 
-    /// Path to map file
-    #[arg(value_name = "PATH")]
+    /// MAP_FILE_PATH to parse
+    #[arg(value_name = "MAP_FILE_PATH")]
     path: String,
+
+    /// OUTPUT_PATH(Optional) default is empty for current path
+    #[arg(short = 'o', long, value_name = "OUTPUT_PATH", default_value = "")]
+    output_path: Option<String>,
+   
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.path);
-    }
 
     if !fs::metadata(&args.path).is_ok() {
         eprintln!("Error: File at path '{}' does not exist.", args.path);
@@ -52,10 +61,10 @@ fn main() {
 
     // Start parsing the file
     println!("Parsing file at path: {}", args.path);
-    parse_map_file(&args.path);
+    parse_map_file(&args.path, args.output_path.as_deref().unwrap_or(""), args.json);
 }
 
-fn parse_map_file(file_path: &str) {
+fn parse_map_file(map_path: &str, out_path: &str, json: bool) {
     // Define an enum for parser states
     #[derive(Debug)]
     enum ParserState {
@@ -67,10 +76,10 @@ fn parse_map_file(file_path: &str) {
     }
 
     // Open the file
-    let file = match File::open(file_path) {
+    let file = match File::open(map_path) {
         Ok(file) => file,
         Err(e) => {
-            eprintln!("Error opening file {}: {}", file_path, e);
+            eprintln!("Error opening file {}: {}", map_path, e);
             std::process::exit(1);
         }
     };
@@ -79,21 +88,8 @@ fn parse_map_file(file_path: &str) {
 
     let mut archive_members = Vec::new();
     let mut discarded_sections = Vec::new();
-    let mut memory_configuration = Vec::new();
+    let mut memory_configuration: Vec<MemoryConfig> = Vec::new();
     let mut linker_script_memory_map: Vec<MemoryMap> = Vec::new();
-    // let mut memory_map: MemoryMap = MemoryMap {
-    //     section: String::new(),
-    //     section_address: String::new(),
-    //     section_length: String::new(),
-    //     sub_section: Vec::new()
-    // };
-    // println!("Memory Map: {:?}{:?}", memory_map.section, memory_map.section.is_empty());
-    // println!("Memory Map: {:?}{:?}", memory_map.sub_section, memory_map.sub_section.is_empty());
-    // println!("Memory Map: {:?}{:?}", memory_map.address, memory_map.address.is_empty());
-    // println!("Memory Map: {:?}{:?}", memory_map.length, memory_map.length == 0);
-    // println!("Memory Map: {:?}{:?}", memory_map.object, memory_map.object.is_empty());
-    // println!("Memory Map: {:?}{:?}", memory_map.demangled, memory_map.demangled.is_empty());
-    // let mut linker_script_memory_map_section = Vec::new();
 
     let mut parser_state = ParserState::None;
     let mut memory_map_parser_state = 0;
@@ -149,8 +145,19 @@ fn parse_map_file(file_path: &str) {
                 }
             }
             ParserState::MemoryConfiguration => {
-                if !line.trim().is_empty() {
-                    memory_configuration.push(line.split_whitespace().collect::<Vec<_>>().join("\t"));
+                if !(line.trim().is_empty()
+                    || line.starts_with("*default*")
+                    || line.starts_with("Name             Origin             Length             Attributes")
+                ) {
+                    
+                    let mut split_line = line.split_whitespace();
+                    let mem_config = MemoryConfig {
+                        name: split_line.next().unwrap_or("").to_string(),
+                        origin: split_line.next().unwrap_or("").to_string(),
+                        length: split_line.next().unwrap_or("").to_string(),
+                        attribute: split_line.collect::<Vec<_>>().join(" ")
+                    };
+                    memory_configuration.push(mem_config);
                 }
             }
             ParserState::LinkerScriptMemoryMap => {
@@ -358,8 +365,6 @@ fn parse_map_file(file_path: &str) {
                         }
 
                     }
-
-                    // println!("{} {} {} {} {}", section, sub_section, address, length, object);
                 }
             }
             ParserState::None => {
@@ -419,106 +424,126 @@ fn parse_map_file(file_path: &str) {
 
     if !memory_configuration.is_empty() {
         println!("\nMemory Configuration:");
-        // Save memory_configuration to a plain text file with a .csv extension
-        let output_file_path = "memory_configuration.tsv";
-        let mut file = match OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(output_file_path)
-        {
-            Ok(file) => file,
-            Err(e) => {
-                eprintln!("Error creating file {}: {}", output_file_path, e);
-                return;
+        if json {
+            match serde_json::to_string(&memory_configuration) {
+            Ok(json) => {
+                let output_file_path = "memory_configuration.json";
+                let mut file = match OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(output_file_path)
+                {
+                Ok(file) => file,
+                Err(e) => {
+                    eprintln!("Error creating file {}: {}", output_file_path, e);
+                    return;
+                }
+                };
+
+                if let Err(e) = writeln!(file, "{}", json) {
+                    eprintln!("Error writing to file {}: {}", output_file_path, e);
+                    return;
+                }
+                println!("Memory configuration saved to {}", output_file_path);
             }
-        };
-    
-        for memory in memory_configuration {
-            // println!("{}", memory); //Debug print
-            if let Err(e) = writeln!(file, "{}", memory) {
-                eprintln!("Error writing to file {}: {}", output_file_path, e);
-                return;
+            Err(e) => eprintln!("Error converting memory_configuration to JSON: {}", e),
             }
-        }
-        println!("Memory configuration saved to {}", output_file_path);
-    }
-
-    // println!("\nLinker Script and Memory Map:");
-    
-    // Convert linker_script_memory_map to JSON and print it
-    // match serde_json::to_string(&linker_script_memory_map) {
-    //     // Ok(json) => println!("{}", json),
-    //     Ok(json) => {
-    //         let output_file_path = "linker_script_memory_map.json";
-    //         let mut file = match OpenOptions::new()
-    //         .write(true)
-    //         .create(true)
-    //         .truncate(true)
-    //         .open(output_file_path)
-    //         {
-    //         Ok(file) => file,
-    //         Err(e) => {
-    //             eprintln!("Error creating file {}: {}", output_file_path, e);
-    //             return;
-    //         }
-    //         };
-
-    //         if let Err(e) = writeln!(file, "{}", json) {
-    //         eprintln!("Error writing to file {}: {}", output_file_path, e);
-    //         return;
-    //         }
-
-    //         println!("Linker Script and Memory Map saved to {}", output_file_path);
-    //     },
-    //     Err(e) => eprintln!("Error converting linker_script_memory_map to JSON: {}", e),
-    // }
-
-    // for map in linker_script_memory_map {
-    //     println!("--- {} {} {} ---", map.section, map.section_address, map.section_length);
-    //     for sub_section in map.sub_section {
-    //         println!("  {} {} {} {} {}", sub_section.name, sub_section.address, sub_section.length, sub_section.object, sub_section.demangled.join(","));
-    //     }
-    if !linker_script_memory_map.is_empty() {
-        println!("\nLinker Script and Memory Map:");
-        let output_file_path = "memory_map.tsv";
-        let mut file = match OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(output_file_path)
-        {
-            Ok(file) => file,
-            Err(e) => {
-                eprintln!("Error creating file {}: {}", output_file_path, e);
-                return;
-            }
-        };
-
-        for map in &linker_script_memory_map {
-            let section = &map.section;
-            // let section_address = &map.section_address;
-            // let section_length = &map.section_length;
-
-            for sub in &map.sub_section {
-                let sub_name = sub.name.join(" ");
-                let sub_address = &sub.address;
-                let sub_length = &sub.length;
-                let sub_object = &sub.object;
-                let sub_demangled = sub.demangled.join(" ");
-
-                if let Err(e) = writeln!(
-                    file,
-                    // "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
-                    "{}\t{}\t{}\t{}\t{}\t{}",
-                    section, /*section_address, section_length,*/
-                    sub_name, sub_address, sub_length, sub_object, sub_demangled
-                ) {
+        } else {
+            // Save memory_configuration to a plain text file with a .csv extension
+            let output_file_path = "memory_configuration.tsv";
+            let mut file = match OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(output_file_path)
+            {
+                Ok(file) => file,
+                Err(e) => {
+                    eprintln!("Error creating file {}: {}", output_file_path, e);
+                    return;
+                }
+            };
+        
+            for memory in memory_configuration {
+                // println!("{}", memory); //Debug print
+                if let Err(e) = writeln!(file, "{}\t{}\t{}\t{}", memory.name, memory.origin, memory.length, memory.attribute) {
                     eprintln!("Error writing to file {}: {}", output_file_path, e);
                     return;
                 }
             }
+            println!("Memory configuration saved to {}", output_file_path);
         }
-        println!("Linker Script and Memory Map saved to {}", output_file_path);
+
+    }
+    
+    if !linker_script_memory_map.is_empty() {
+        println!("\nLinker Script and Memory Map:");
+        if json {
+            match serde_json::to_string(&linker_script_memory_map) {
+                // Ok(json) => println!("{}", json),
+                Ok(json) => {
+                    let output_file_path = "linker_script_memory_map.json";
+                    let mut file = match OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .truncate(true)
+                    .open(output_file_path)
+                    {
+                    Ok(file) => file,
+                    Err(e) => {
+                        eprintln!("Error creating file {}: {}", output_file_path, e);
+                        return;
+                    }
+                    };
+        
+                    if let Err(e) = writeln!(file, "{}", json) {
+                    eprintln!("Error writing to file {}: {}", output_file_path, e);
+                    return;
+                    }
+                    println!("Linker Script and Memory Map saved to {}", output_file_path);
+                },
+                Err(e) => eprintln!("Error converting linker_script_memory_map to JSON: {}", e),
+            }
+        } else {
+            let output_file_path = "memory_map.tsv";
+            let mut file = match OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(output_file_path)
+            {
+                Ok(file) => file,
+                Err(e) => {
+                    eprintln!("Error creating file {}: {}", output_file_path, e);
+                    return;
+                }
+            };
+    
+            for map in &linker_script_memory_map {
+                let section = &map.section;
+    
+                for sub in &map.sub_section {
+                    let sub_name = sub.name.join(" ");
+                    let sub_address = &sub.address;
+                    let sub_length = &sub.length;
+                    let sub_object = &sub.object;
+                    let sub_demangled = sub.demangled.join(" ");
+    
+                    if let Err(e) = writeln!(
+                        file,
+                        // "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+                        "{}\t{}\t{}\t{}\t{}\t{}",
+                        section, /*section_address, section_length,*/
+                        sub_name, sub_address, sub_length, sub_object, sub_demangled
+                    ) {
+                        eprintln!("Error writing to file {}: {}", output_file_path, e);
+                        return;
+                    }
+                }
+            }
+            println!("Linker Script and Memory Map saved to {}", output_file_path);
+        }
+        
     }
 }
